efcf7d95d154eecf54e5aa062a29810947c3d583
Yoanis Gil Delgado <gil.yoanis@gmail.com>
2015-11-17 17:34:28 -0500

    POC for merging consumers

--- src/mergeUtils.js
+++ src/mergeUtils.js
@@ -3,7 +3,7 @@
 import merge from 'merge';
 import _ from 'underscore';
 
-export default function mergeConfig(baseConfig, overrideConfig) {
+export default function mergeConfig(baseConfig, overrideConfig, path = '') {
     if (!baseConfig) {
         return;
     }
@@ -21,6 +21,8 @@ export default function mergeConfig(baseConfig, overrideConfig) {
             continue;
         }
 
+        var currentPath = appendKeyToPath(path, key);
+
         if (_.isArray(baseConfig[key])) {
             var elements = merge.clone(baseConfig[key]);
             var overrideElements = overrideConfig[key];
@@ -31,8 +33,10 @@ export default function mergeConfig(baseConfig, overrideConfig) {
                 var pos = -1;
                 var matchingElement = null;
 
+                var lookupKey = keyForPath(currentPath);
+
                 for (var j = 0; j < baseConfig[key].length; j++) {
-                    if (currentElement.name == baseConfig[key][j].name) {
+                    if (currentElement[lookupKey] == baseConfig[key][j][lookupKey]) {
                         pos = j;
                         matchingElement = baseConfig[key][j];
                         break;
@@ -40,7 +44,7 @@ export default function mergeConfig(baseConfig, overrideConfig) {
                 }
 
                 if (pos != -1) {
-                    elements[pos] = mergeConfig(matchingElement, currentElement);
+                    elements[pos] = mergeConfig(matchingElement, currentElement, currentPath);
                 } else {
                     elements.push(currentElement);
                 }
@@ -48,11 +52,21 @@ export default function mergeConfig(baseConfig, overrideConfig) {
 
             mergedConfig[key] = elements;
         } else if (_.isObject(baseConfig[key])) {
-            mergedConfig[key] = mergeConfig(baseConfig[key], overrideConfig[key]);
+            mergedConfig[key] = mergeConfig(baseConfig[key], overrideConfig[key], currentPath);
         } else {
             mergedConfig[key] = overrideConfig[key];
         }
     }
 
     return mergedConfig;
+};
+
+let keyForPath = (path) => {
+    var [first, ...rest] = path.split('.');
+
+    return ('consumers' === first && _.isEmpty(rest)) ? 'username' : 'name';
+};
+
+let appendKeyToPath = (path, key) => {
+    return _.isEmpty(path) ? key : path + '.' + key;
 };
