2303a7135bf32314a32c08330dc865e1f92bf1b1
Sten Hiedel <r.sten@hiedel.ee>
2015-12-04 16:33:22 +0000

    Fix BC for default command to allow kongfig --path ...

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ let pkg = require("../package.json");
 
 commander
     .version(pkg.version)
+    .allowUnknownOption()
     .command('apply', 'Apply config to a kong server', {isDefault: true})
     .command('dump', 'Dump the configuration from a kong server')
     .parse(process.argv);
